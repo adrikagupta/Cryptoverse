4b9869dd1f842fcc2b3f650afb1ec47ad739e319
Adrika Gupta <adrikagupta22@gmail.com>
2022-05-07 18:43:06 +0530

    news page made

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import millify from 'millify'
+import HTMLReactParser from 'html-react-parser';
+import {Row,Col,Typography,Select} from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons'
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+
 
 const CryptoDetails = () => {
+  const {coinId} = useParams()
+  const {data, isFetching} = useGetCryptoDetails(coinId);
+  
   return (
-    <div>CryptoDetails</div>
+    <div>CryptoDetails {coinId}</div>
   )
 }
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,11 +15,14 @@ export const cryptoApi = createApi({
     endpoints : (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
         })
     })
 })
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery , useGetCryptoDetailsQuery} = cryptoApi;
 
 /*
 redux allows us to create hooks to get data for the query; loadig states; finalized states;
